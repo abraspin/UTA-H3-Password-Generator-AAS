adfa2c3b84621e2538280b38d3404ac8ef39c1d3
abraspin <abrahamspindel@gmail.com>
2020-08-06 01:24:00 -0500

    update password length input error checking-it works!

--- linked-docs/script.js
+++ linked-docs/script.js
@@ -3,18 +3,14 @@ var generateBtn = document.querySelector("#generate");
 var hasSelectedType; //This boolean tracks whether the user has not selected any character types
 
 // Declaration of character arrays for RNG to choose from.
-var upperCharArray = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split(
-  " "
-);
+var upperCharArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(  "");
 console.log(upperCharArray);
-var lowerCharArray = "a b c d e f g h i j k l m n o p q r s t u vw x y z".split(
-  " "
-);
-var numericalCharArray = "0 1 2 3 4 5 6 7 8 9".split(" ");
-// I'm omitting spaces, quotes, and slashes so they don't accidentally mess up wherever it gets passed
-// I realized after the last 3 variables you can leave the space seperating value and just pass split an empty string ""
+var lowerCharArray = "abcdefghijklmnopqrstuvwxyz".split(  "");
+var numericalCharArray = "0123456789".split("");
+// I'm omitting spaces, quotes, and slashes from special characters so they don't accidentally mess up wherever it gets passed
 var specialCharArray = "!#$%&'()*+,-.:;<=>?@[]^_`{|}~".split("");
-console.log(specialCharArray);
+// console.log(specialCharArray);
+//initializing empty array that will eventually contain pool of characters to build new password
 var charPool = [];
 
 // my extremely lazy function to post charPool to console
@@ -25,10 +21,10 @@ function logCharPool() {
 // my function, checks if the user wants a type of character included in
 //their password and if they do, it appends that character
 // type's pool array on to the master charPool
-function pushCharArray(wantsIncluded, charArray) {
-  if (wantsIncluded.toLowerCase() === "yes") {
 
-    
+// I need to check to make sure they don't hit cancel, which returns "null"
+function pushCharArray(wantsIncluded, charArray) {
+  if (wantsIncluded !== null && wantsIncluded.toLowerCase() === "yes") {
     for (var i = 0; i<charArray.length; i++){
       charPool.push(charArray[i]);
     }
@@ -36,73 +32,99 @@ function pushCharArray(wantsIncluded, charArray) {
   }
 }
 
-//not that smart actually thi is just to fix the mess up above.
-// Ok I'm definitely a genius, this function loops through 
-//the chosen char type array and adds each element one by 
-//one to the charPool. I wish there was a way to concatenate 
-//entire arrays but the interne seemed iffy on this.
-
-
 ////////////////////////////////////////////////////////////////////////
 
-///I think the actual calling of this function ...needs this function to return the completed password.
+// This function asks the user to enter their password length, 
+//and choose which type of character they want included in 
+//their new password (upper case, lower case, numbers, special characters)
+//Error handling: 
+///////Will escape function if no or bad password length is entered, or
+///////fewer than 1 type of character is selected
+////////WILL NOT alert user if they have entered a char type OTHER THAN "yes" or "no"
 function generatePassword() {
+  //Ask the user how long they want their password to be, and check it is between 8 and 128. 
   var newPassword = "";
+
   var passwordLength = prompt(
-    "What length would you like your new password to be? (Please choose a value between 1 and 128)"
-  );
+    "What length would you like your new password to be? (Please choose a whole number between 8 and 128)"  );
   console.log("password length: " + passwordLength);
+  // This error checking confirms range but also ensures a whole number  has been entered, 
+console.log("typeof passwordLength: " + typeof passwordLength)
+  console.log("is int true or false? "+parseInt(passwordLength));
 
-  if (passwordLength > 1 && passwordLength < 128) {
-    // Will add error handling later, nest each in an if statement checking for correct input
-    // For each character type the user chooses, the array of all available of that category will be added to the array representing the pool of available random characters
+console.log("Number.isInteger(parseInt(passwordLength))---" + Number.isInteger(parseInt(passwordLength)))
+
+passwordLength = parseInt(passwordLength);
+console.log("line 58 passwordLength----" +passwordLength)
+//this peculiar conditional parses the user input to an int. If it is passed a NaN or empty string it will be false...I think
+  if (parseInt(passwordLength) &&
+  passwordLength >= 8 && 
+  passwordLength <= 128) {
     alert(
-      "The next few prompts will ask about the special characters you want to be present in your new passwords."
-    );
+      "The next few prompts will ask about the special characters you want to be present in your new passwords."    );
 
+
+//Ask user if they want Capitalized Letters included in their new password
     var useUpperCase = prompt(
-      "Would you like your new passsword to include \r\nCapitalized Letters? \r\nType 'yes' or 'no'"
-    );
-
+      "Would you like your new passsword to include \r\nCapitalized Letters? \r\nType 'yes' or 'no'"    );
     pushCharArray(useUpperCase, upperCharArray);
 
+
+
+//Ask user if they want Lower case Letters included in their new password
+///////EXPERIMENTAL ERROR HANDLING FOR INDIVIDUAL CHAR TYPE PROMPT
     var useLowerCase = prompt(
-      "Would you like your new passsword to include \r\nLowercase Letters? \r\nType 'yes' or 'no'"
-    );
-    if (useLowerCase.toLowerCase() === "yes") {
+      "Would you like your new passsword to include \r\nLowercase Letters? \r\nType 'yes' or 'no'"    );
+    // if (useLowerCase.toLowerCase() === "yes") {
       pushCharArray(useLowerCase, lowerCharArray);
-    }
+    // }
+    // else {
+    //   alert("Invalid entry detected. Please input EITHER 'yes' OR 'no'. The program will now exit.");
+    //   return;
+    // }
+
+    
+//Ask user if they want Numeric Characters included in their new password
     var useNumericChars = prompt(
-      "Would you like your new passsword to include \r\nNumeric Characters? \r\nType 'yes' or 'no'"
-    );
-    if (useNumericChars.toLowerCase() === "yes") {
+      "Would you like your new passsword to include \r\nNumeric Characters? \r\nType 'yes' or 'no'"    );
+    // if (useNumericChars.toLowerCase() === "yes") {
       pushCharArray(useNumericChars, numericalCharArray);
-    }
+    // }
+    // else {
+    //   alert("Invalid entry detected. Please input EITHER 'yes' OR 'no'. The program will now exit.");
+    //   return;
+    // }
+
+
+//Ask user if they want Special  Characters included in their new password
     var useSpecialChars = prompt(
-      "Would you like your new passsword to include \r\nSpecial Character? \r\nType 'yes' or 'no'"
-    );
-    if (useSpecialChars.toLowerCase() === "yes") {
+      "Would you like your new passsword to include \r\nSpecial Character? \r\nType 'yes' or 'no'"    );
+    // if (useSpecialChars.toLowerCase() === "yes") {
       pushCharArray(useSpecialChars, specialCharArray);
-    }
+    // }
+    // else {
+    //   alert("Invalid entry detected. Please input EITHER 'yes' OR 'no'. The program will now exit.");
+    //   return;
+    // }
 
-    console.log(
-      "each answer: " + useLowerCase,
-      useUpperCase,
-      useSpecialChars,
-      useNumericChars
-    );
-
-    //maybe I can jut check to see if charPool is empty idk
-    // wait is this necessary? (later: uh yes i think) can I just reroute all the conditionals to the final else for invalid entry?
-    // If no types have been selected, it will exit the program here. the only way for this conditional to be true is if all 4 are empty (or null?)
+alert("You have made the following selections: " +
+"\nPassword Length--------" + passwordLength +
+"\nUpper Case Characters--" + useUpperCase +
+"\nLower Case Character---" + useLowerCase +
+"\nNumerical Characters---" + useNumericChars +
+"\nSpecial Characters-----" + useSpecialChars);
+    // If no character types have been selected, it will exit the program here and return an empty string
+    //The boolean check will be set to true if any of the 4 char types are selected.
     if (hasSelectedType) {
-      // loop through as many times as the user wants their password length to be, and spit out a random character from a random category
+      // loop through as many times as the user wants their password length to be, and 
+      //spit out and append a random character from the master pool (random category of the types selected by the user)
       for (var i = 0; i < passwordLength; i++) {
         newPassword += charPool[Math.floor(Math.random() * charPool.length)];
+        console.log(Math.floor(Math.random() * charPool.length))
+       console.log(newPassword)
       }
-      console.log("new password is: "+newPassword);
-
       return newPassword;
+
     } else if (!hasSelectedType) {
       alert(
         "Invalid Entry. Please select at least one character type. This program will now end. Please click the 'Generate Password' button to try again."
@@ -110,7 +132,8 @@ function generatePassword() {
     }
   } else {
     alert(
-      "Invalid character entered. This program will now end. Please click the 'Generate Password' button to try again."
+      "\nInvalid character entered. \n\nPlease enter a whole number between 8 and 128." +
+       "\n\nThis program will now end. \n\nPlease click the 'Generate Password' button to try again."
     );
   }
 }
@@ -120,13 +143,18 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  console.log("generatePassword function is returning: "+ password)
+  //The conditional is Abraham's addition so the password doesn't 
+  //become the word "undefined" on-screen if something goes awry in the prompts. 
+  //This works because it would then be passed a falsy value of NaN or undefined or "". Nifty!
+
+  console.log("typeof password: "+typeof password)
+  console.log("password: "+ password)
+  // if (!password) {passwordText.value="YOUR PASSWORD WAS NOT GENERATED." +
+  // "\nPLEASE DOUBLE CHECK YOUR INPUTS AND TRY AGAIN."} else
   passwordText.value = password;
 }
 
 // Add event listener to generate button
 
-// *************************THE GENERATE BUTTON WILL NOT WORK UNTIL YOU UNCOMMENT 4 LINES DOWN
-//The comment below is the correct code that came with the assignment, 2 below is my tester for the generatePassword() function
-// the generateBtn var is declared at the top, it's assigned to the #generate button element
 generateBtn.addEventListener("click", writePassword);
-// generateBtn.addEventListener("click", generatePassword());
